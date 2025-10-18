f7eff652608cb9abac09d6a8fd8f8d6a2d546254
Albina <alb.mslva@gmail.com>
2025-10-18 13:53:46 +0400

    refactor(registration) : convert registration function to class-based structure

--- junitcompass/users/views.py
+++ junitcompass/users/views.py
@@ -1,8 +1,6 @@
-from django.contrib.auth import  logout
-from django.contrib.auth.views import LoginView, LogoutView
-from django.http import  HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from users.forms import LoginUserForm, RegisterUserForm
 
@@ -12,14 +10,8 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
